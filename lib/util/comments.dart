8f56e7b58177767b21ae7c13b0c92542da81be36
pukkysasikan <sasikan.in.62@ubu.ac.th>
2021-12-22 15:45:18 +0700

    first commit

---
+++ lib/util/comments.dart
@@ -0,0 +1,22 @@
+List comments = [
+  {
+    "img": "assets/cm1.jpeg",
+    "comment": "อร่อยค่ะ",
+    "name": "เหนือ"
+  },
+  {
+    "img": "assets/cm2.jpeg",
+    "comment": "ปัง",
+    "name": "อัยย์"
+  },
+  {
+    "img": "assets/cm3.jpeg",
+    "comment": "สุดยอด",
+    "name": "กี้"
+  },
+  {
+    "img": "assets/cm2.jpeg",
+    "comment": "ปังปุริเย่",
+    "name": "อัยย์"
+  }
+];
---
+++ lib/util/foods.dart
@@ -0,0 +1,47 @@
+List<Map> foods = [
+
+  {
+    "img": "assets/food2.jpeg",
+    "name": "สลัดผลไม้"
+  },
+  {
+    "img": "assets/food3.jpeg",
+    "name": "Hamburger"
+  },
+  {
+    "img": "assets/food4.jpeg",
+    "name": "สลัดผลไม้"
+  },
+  {
+    "img": "assets/food5.jpeg",
+    "name": "Hamburger"
+  },
+  {
+    "img": "assets/food6.jpeg",
+    "name": "สเต็ก"
+  },
+  {
+    "img": "assets/food7.jpeg",
+    "name": "พิซซ่า"
+  },
+  {
+    "img": "assets/food8.jpeg",
+    "name": "หน่อไม้ฝรั่ง"
+  },
+  {
+    "img": "assets/food9.jpeg",
+    "name": "สลัด"
+  },
+  {
+    "img": "assets/food10.jpeg",
+    "name": "พิซซ่า"
+  },
+  {
+    "img": "assets/food11.jpeg",
+    "name": "พิซซ่า"
+  },
+  {
+    "img": "assets/food12.jpg",
+    "name": "สลัด"
+  },
+];
